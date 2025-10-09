d3dba02f29bd19aa5b2cb3753d20c0e14db47949
Tech VJ <122880036+VJBots@users.noreply.github.com>
2025-10-09 14:20:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,13 @@
 # Ask Doubt on telegram @KingVJ01
 
 from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN
+from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION, LOGIN_SYSTEM
+
+if STRING_SESSION is not None and LOGIN_SYSTEM == False:
+	TechVJUser = Client("TechVJ" ,api_id=api_id, api_hash=api_hash, session_string=STRING_SESSION)
+	TechVJUser.start()
+else:
+    TechVJUser = None
 
 class Bot(Client):
 
@@ -14,8 +20,8 @@ class Bot(Client):
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             plugins=dict(root="TechVJ"),
-            workers=50,
-            sleep_threshold=10
+            workers=150,
+            sleep_threshold=5
         )
 
       
