83ad4361e93e4b22879c5d9c23432e5a86044845
Tech VJ <122880036+VJBots@users.noreply.github.com>
2025-10-27 21:37:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-# Don't Remove Credit Tg - @VJ_Botz
+# Don't Remove Credit Tg - @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
@@ -6,7 +6,7 @@ from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN, STRING_SESSION, LOGIN_SYSTEM
 
 if STRING_SESSION is not None and LOGIN_SYSTEM == False:
-	TechVJUser = Client("TechVJ" ,api_id=api_id, api_hash=api_hash, session_string=STRING_SESSION)
+	TechVJUser = Client("TechVJ" ,api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION)
 	TechVJUser.start()
 else:
     TechVJUser = None
@@ -28,7 +28,7 @@ class Bot(Client):
     async def start(self):
             
         await super().start()
-        print('Bot Started Powered By @VJ_Botz')
+        print('Bot Started Powered By @VJ_Bots')
 
     async def stop(self, *args):
 
@@ -37,6 +37,6 @@ class Bot(Client):
 
 Bot().run()
 
-# Don't Remove Credit Tg - @VJ_Botz
+# Don't Remove Credit Tg - @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
